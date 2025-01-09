681ee652a01601aa01e7161d7f7bca0b449fd9e4
Shadab Anwar <shadabanwar1810@gmail.com>
2025-01-09 22:22:23 +0530

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,8 +1,7 @@
-x = int(input("Enter your number 1 :"))
-if (x%2==0):
-  print("x is a even number")
-elif(x == 0):
-  print("0 is neither even nor odd")
+x = int(input("Enter your number 1: "))
+if x == 0:
+    print("0 is neither even nor odd")
+elif x % 2 == 0:
+    print("x is an even number")
 else:
-  print("x is a odd number")
-
+    print("x is an odd number")
