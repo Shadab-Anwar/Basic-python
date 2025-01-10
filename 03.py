f52c809e5c8004a4660833d7e8f9d79cd4f33653
Shadab Anwar <shadabanwar1810@gmail.com>
2025-01-10 17:14:17 +0530

    updated 3

--- 03.py
+++ 03.py
@@ -1,8 +1,11 @@
 a = int(input("Enter your number :"))
-k = 1
-pro = 1
-while (k <= a):
-        pro = pro * k
-        k = k + 1
-print(pro)       
+if (a<0):
+        print("you have entered a -ve num")
+else:
+        k = 1
+        pro = 1
+        while (k <= a):
+                pro = pro * k
+                k = k + 1
+        print(pro)       
     
