26f1fb121d729d73178de2b12024aa3c31d83b36
Shadab Anwar <shadabanwar1810@gmail.com>
2025-01-28 10:07:25 +0530

    Update 01.py

--- 5thclass/01.py
+++ 5thclass/01.py
@@ -2,13 +2,10 @@ from sklearn.model_selection import KFold, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 import pandas as pd
-
 iris = load_iris()
-
 iris_df = pd.DataFrame(iris.data, columns=iris.feature_names)
 iris_df['target'] = iris.target
 print(iris_df.head())
-
 X, y = iris.data, iris.target
 model = RandomForestClassifier()
 kfold = KFold(n_splits=5, shuffle=True, random_state=42)
